1a9c40fc3e2db651d0c9643161a495d80319ae23
ktscates <cates2506@gmail.com>
2019-08-25 15:43:48 +0200

    Update quote class, app component and quote form component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,8 @@
 export class Quote {
+    public showDescription: boolean;
+
+    constructor(public id: number, public quote: string, public author: string, public person: string){
+        this.showDescription = false;
+    }
+
 }
