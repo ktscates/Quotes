f3f351bf8b8e8e31d2097e4765f6acda27acb3a1
ktscates <cates2506@gmail.com>
2019-08-25 15:27:05 +0200

    Add quote-detail component and update app.module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailComponent } from './quote/quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
