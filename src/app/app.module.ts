d1bd1da19868d0f1cca873e6904534c6ff4946fc
ktscates <cates2506@gmail.com>
2019-08-26 10:37:51 +0200

    Minor Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteDetailComponent } from './quote/quote-detail/quote-detail.component';
-import { LikeComponent } from './like/like.component';
 import { HighlightDirective } from './highlight.directive';
 
 
@@ -18,7 +17,6 @@ import { HighlightDirective } from './highlight.directive';
     QuoteComponent,
     QuoteFormComponent,
     QuoteDetailComponent,
-    LikeComponent,
     TimeAgoPipe,
     HighlightDirective,
   ],
